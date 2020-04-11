4345e91b71b0e695b1248db38f8b942652a21e21
hoya-1004 <whtjdgh104@naver.com>
2020-04-11 17:48:38 +0900

    study

--- Car.java
+++ Car.java
@@ -1,8 +1,27 @@
 public class Car {
     // 필드
     String company = "현대 자동차";
-    String model = "그랜져";
-    String color = "검정";
-    int maxSpeed = 350;
-    int speed;
+    String model;
+    String color;
+    int maxSpeed;
+
+    // 생성자
+    Car() {
+
+    }
+
+    Car(String model) {
+        this.model = model;
+    }
+
+    Car(String model, String color) {
+        this.model = model;
+        this.color = color;
+    }
+
+    Car(String model, String color, int maxSpeed) {
+        this.model = model;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+    }
 }
