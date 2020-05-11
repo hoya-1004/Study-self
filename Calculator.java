c85570209c2b45a29240504b4204941db904b29c
hoya-1004 <whtjdgh104@naver.com>
2020-05-11 23:48:23 +0900

    study

--- Calculator.java
+++ Calculator.java
@@ -1,22 +1,11 @@
 public class Calculator {
+    static double pi = 3.14159;
 
-    int plus(int x, int y) {
-        int result = x + y;
-        return result;
+    static int plus(int x, int y) {
+        return x + y;
     }
 
-    double avg(int x, int y) {
-        double sum = plus(x, y);
-        double result = sum / 2;
-        return result;
-    }
-
-    void execute() {
-        double result = avg(7, 10);
-        System.out.println("실행결과 : " + result);
-    }
-
-    void println(String message) {
-        System.out.println(message);
+    static int minus(int x, int y) {
+        return x - y;
     }
 }
